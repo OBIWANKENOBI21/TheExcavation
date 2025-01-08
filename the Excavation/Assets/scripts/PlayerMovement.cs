b7fc2a2d1407dcb8b34f0fcc301c4bb32ad30db0
OBIWANKENOBI21 <dannydownie1746@aol.com>
2025-01-08 15:59:42 +0000

    ground sensor
    added ground sensor and jump button

---
+++ the Excavation/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D physicsbody = null;
+    public float speed = 4f;
+    // jump speed setting, in meters/second
+    public float jumpSpeed = 6f;
+    public Collider2D groundSensor = null;
+    public LayerMask groundLayer = 0;
+
+    // Awake is called before start
+    private void Awake()
+    {
+        physicsbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void MoveLeft()
+    {
+        // physicsbody.velocity.x = -1;
+
+        // male a variable to hold the velocity
+        // get the velocity from the rigidbody2D
+        Vector2 newVelocity = physicsbody.velocity;
+
+        newVelocity.x = -2.5f;
+
+        physicsbody.velocity = newVelocity;
+
+    }
+    public void MoveRight()
+    {
+        // physicsbody.velocity.x = -1;
+
+        // male a variable to hold the velocity
+        // get the velocity from the rigidbody2D
+        Vector2 newVelocity = physicsbody.velocity;
+
+        newVelocity.x = 2.5f;
+
+        physicsbody.velocity = newVelocity;
+
+    }
+    public void Jump()
+    {
+        // input to allow groundSensor's to work
+        if (groundSensor.IsTouchingLayers(groundLayer))
+        {
+            // a varible to hold the velocity and physics component
+            Vector2 newVelocity = physicsbody.velocity;
+
+            // setting the velcity to move in postive y (UP) direction
+            newVelocity.y = jumpSpeed;
+
+            // update for the physics component's velocity to the changed value
+            physicsbody.velocity = newVelocity;
+        }
+    }
+
+     
+    
+
+
+
+
+}
